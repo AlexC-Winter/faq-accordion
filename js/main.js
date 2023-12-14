04c4108a547e594014027e69e5abb2a186773e3c
Alex_W <alexcwinter7@gmail.com>
2023-12-13 18:25:46 -0600

    js file and functions added

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const faqs = document.querySelectorAll(".qa-pairs");
+
+faqs.forEach((faq) => {
+  const button = faq.querySelector("h2");
+  const image = faq.querySelector("h2 img");
+  const paragraph = faq.querySelector("p");
+
+  button.addEventListener("click", function() {
+    if (paragraph.classList.contains("hidden")) {
+      paragraph.classList.remove("hidden");
+      image.src = "assets/images/icon-minus.svg";
+    } else {
+      paragraph.classList.contains("hidden");
+      image.src = "assets/images/icon-plus.svg";
+    }
+  });
+});
